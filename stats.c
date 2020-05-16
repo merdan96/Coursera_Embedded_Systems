aad2f5ab751040fe7eb48b028da35c8de25257c4
merdan96 <merdan96@yahoo.com>
2020-05-16 05:48:48 -0600

    Added the main function testing code

--- stats.c
+++ stats.c
@@ -38,6 +38,14 @@ void main() {
                                 201,   6,  12,  60,   8,   2,   5,  67,
                                  7,  87, 250, 230,  99,   3, 100,  90};
 
+	uint8_t min, max, mean, median;
+
+	min = find_minimum(test, SIZE);
+	max = find_maximum(test, SIZE);
+	mean = find_mean(test, SIZE);
+	median = find_median(test, SIZE);
+	
+	print_statistics(min, max, mean, median);
 
 }
 
